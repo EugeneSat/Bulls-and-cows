a6373e6cd2ad143dbaeac11dc4035dc201216d1a
EugeneSat <95920032+EugeneSat@users.noreply.github.com>
2022-01-17 03:50:50 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const button = document.querySelector(".button");
 const todo = document.querySelector(".todo");
 const your_number = document.querySelector(".input");
 let k,b;
+
+your_number.oninput = function(){
+  this.value = this.value.substr(0,4);
+}
 function Random(num_a,num_b) {
   return Math.floor( Math.random() * ((num_b - num_a + 1) + num_a));
 }
@@ -50,10 +54,12 @@ Analize(master, kkk.todo);
 //добавляем числа в массив,которые введи в инпут
 enterList.push(kkk);
 
+
+
 DisplayEnter();
 //если значения твое и компьютера совпадают
 if (master == kkk.todo) {
- alert("Поздравляю,Вы выйграли!Это число" + `${master}`);
+ alert("Поздравляю,Вы выйграли!Это число" + " " + `${master}`);
 }
 });
 
@@ -63,16 +69,31 @@ function DisplayEnter() {
 //Переменная для ограничения игры в 10 шагов
 let check = 0;
 let displayMessage = '';
+
  enterList.forEach(function(item, i) {
+    b = b.replace(item, i);
+    k = k.replace();
     displayMessage += `
       <li>
-        <label for='item_${i}'>${item.todo} ${b}Б ${k}K </label>
+        <label for='item_${i}'>${item.todo} ${b}Б ${k}K</label>
         </li>`;
          todo.innerHTML = displayMessage;
+
+
          check++;
  });
+
  //Через 10 шагов игра закончится
  if (check == 10) {
    alert("Игра закончилась,это число было " + `${master}`);
  }
+if (check == 11) {
+   alert("Игра уже закончилась,обновите страницу и попробуйте снова!");
+}
+}
+
+
+//краска инпута
+your_number.onfocus = function(){
+  this.classList.add("input2");
 }
